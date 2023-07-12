9384e390fcb182e81e76c64e8a1a5fe9b0f96adc
David Yepes <djvash01@gmail.com>
2023-07-12 18:33:32 -0500

    feat: 🎸 add  serach input

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { SearchInputComponent } from './search-input/search-input.component';
 
 
 
 @NgModule({
   declarations: [
-    ButtonComponent
+    ButtonComponent,
+    SearchInputComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ButtonComponent
+    ButtonComponent,
+    SearchInputComponent
   ]
 })
 export class SharedModule { }
