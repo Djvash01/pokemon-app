f1d5daf7e0b4db7a031f8b0bc3371a8f46baea21
David Yepes <djvash01@gmail.com>
2023-07-12 19:33:31 -0500

    feat: 🎸 create dasboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from '@shared/shared.module';
+import { ViewsModule } from '@views/views.module';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     SharedModule,
+    ViewsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -7,5 +7,5 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent {
-  @Input() public disabled = true;
+  @Input() public disabled = false;
 }
